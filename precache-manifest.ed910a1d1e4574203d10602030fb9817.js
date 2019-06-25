1bdc1c4f81185040702edb90f1da68603d9d081d
ogunsolahabib <ogunsolahabib@yahoo.com>
2019-06-25 22:42:47 +0100

    Updates

---
+++ precache-manifest.ed910a1d1e4574203d10602030fb9817.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf3271d802e8507673cb6855c9cb988",
+    "url": "/world-countries/index.html"
+  },
+  {
+    "revision": "33d9f9081582f0524b38",
+    "url": "/world-countries/static/js/2.4f9f3b87.chunk.js"
+  },
+  {
+    "revision": "ebf07431517cf71c1e66",
+    "url": "/world-countries/static/js/main.fce81de5.chunk.js"
+  },
+  {
+    "revision": "c9dcdc90cda18b8ba72a",
+    "url": "/world-countries/static/js/runtime~main.c4bc1bef.js"
+  }
+]);
